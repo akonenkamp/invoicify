159ce7e8a82e8353a5309d6e7734c5d7d6adaded
Amanda Konenkamp <amanda.konenkamp@libertymutual.com>
2017-09-13 17:15:31 -0700

    tests updated for user and use role

---
+++ src/test/java/com/libertymutual/goforcode/invoicify/models/UserTests.java
@@ -0,0 +1,56 @@
+package com.libertymutual.goforcode.invoicify.models;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+import org.meanbean.test.BeanTester;
+import org.meanbean.test.Configuration;
+import org.meanbean.test.ConfigurationBuilder;
+
+public class UserTests {
+
+	@Test
+
+	public void checking_to_make_sure_my_getters_and_setters_work() {
+
+		BeanTester tester = new BeanTester();
+
+		Configuration configuration = new ConfigurationBuilder()
+
+				.ignoreProperty("createdOn")
+
+				.build();
+
+		tester.testBean(User.class, configuration);
+
+	}
+	
+	@Test
+	public void test_that_isAccountNonExpired_returns_true() {
+		User user = new User();
+		boolean nonExpired = user.isAccountNonExpired();
+		assertThat(nonExpired).isTrue();
+	}
+	
+	@Test
+	public void test_that_isAccountNonLocked_returns_true() {
+		User user = new User();
+		boolean nonLocked = user.isAccountNonLocked();
+		assertThat(nonLocked).isTrue();
+	}
+	
+	@Test
+	public void test_that_isEnabled_returns_true() {
+		User user = new User();
+		boolean enabled = user.isEnabled();
+		assertThat(enabled).isTrue();
+	}
+	
+	@Test
+	public void test_that_isCredentialNonExpired_returns_true() {
+		User user = new User();
+		boolean nonCredentialExpired = user.isCredentialsNonExpired();
+		assertThat(nonCredentialExpired).isTrue();
+	}
+	
+}
